d8f0458b023dc28d46830ab51aff801b06446199
Shobhit010 <shobhitpoddar410@gmail.com>
2025-07-02 03:57:03 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -1,34 +1,67 @@
-import './config/intrument.js' // Ensure this is imported before any other code
+// import './config/intrument.js' // Ensure this is imported before any other code
+// import express from 'express'
+// import cors from 'cors'
+// import 'dotenv/config'
+// import connectDB from './config/db.js'
+// import * as Sentry from '@sentry/node'
+// import { clerkWebhooks } from './controllers/webhooks.js'; 
+
+// // Initialize Express
+// const app = express()
+
+// // Connect to Database
+// await connectDB()
+
+// // Middlewares
+// app.use(cors())
+// app.use(express.json())
+
+// // Routes
+// app.get('/', (req, res) => {
+//   res.send('Welcome to the API')
+// })
+// app.get("/debug-sentry", function mainHandler(req, res) {
+//   throw new Error("My first Sentry error!");
+// });
+// app.post('/webhooks',clerkWebhooks)
+
+// // Port
+// const PORT = process.env.PORT || 5000
+
+// // Sentry Configuration
+// Sentry.setupExpressErrorHandler(app);
+
+// export default app
+
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/db.js'
 import * as Sentry from '@sentry/node'
-import { clerkWebhooks } from './controllers/webhooks.js'; 
+import { clerkWebhooks } from './controllers/webhooks.js'
+import bodyParser from 'body-parser'
 
-// Initialize Express
 const app = express()
 
-// Connect to Database
 await connectDB()
 
-// Middlewares
 app.use(cors())
+
+// Apply raw body only to /webhooks for Svix to verify
+app.post('/webhooks', bodyParser.raw({ type: 'application/json' }), clerkWebhooks)
+
+// Normal JSON for rest of the app
 app.use(express.json())
 
-// Routes
 app.get('/', (req, res) => {
   res.send('Welcome to the API')
 })
-app.get("/debug-sentry", function mainHandler(req, res) {
-  throw new Error("My first Sentry error!");
-});
-app.post('/webhooks',clerkWebhooks)
 
-// Port
+app.get('/debug-sentry', function mainHandler(req, res) {
+  throw new Error('My first Sentry error!')
+})
+
 const PORT = process.env.PORT || 5000
-
-// Sentry Configuration
-Sentry.setupExpressErrorHandler(app);
+Sentry.setupExpressErrorHandler(app)
 
 export default app
