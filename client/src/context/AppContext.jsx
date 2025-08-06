1b115d13f41d0f59ad9e01c974215086f08b1c9c
Shobhit010 <shobhitpoddar410@gmail.com>
2025-08-06 12:01:20 +0530

    env updated

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -7,7 +7,7 @@ export const AppContext = createContext();
 
 export const AppContextProvider = (props) => {
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = import.meta.env.VITE_BACKEND_URL.replace(/\/+$/, '')
 
     const { user } = useUser()
     const { getToken } = useAuth()
